3e3280a3be6142fecd8d17729b91cb883ff4d3eb
Suvidha <Suvidha@Suvidhas-Mac-mini.local>
2015-01-20 11:42:18 +0530

    commit

--- Pollarise/ViewController.m
+++ Pollarise/ViewController.m
@@ -17,8 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"Commit Changes..");
-        NSLog(@"Commit Changes.. by Akash... another one ,.......");
 }
 
 - (void)didReceiveMemoryWarning {
